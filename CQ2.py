5513859027ff98a5ddb3910090c8e6d3c364a00c
Sneha20Bhardwaj <163544358+Sneha20Bhardwaj@users.noreply.github.com>
2025-07-17 00:21:24 +0530

    Add files via upload

---
+++ CQ2.py
@@ -0,0 +1,9 @@
+#Calculation from Math Module
+import math
+n=float(input("Enter a number : "))
+sqrtRoot=math.sqrt(n)
+print("Square Root :",sqrtRoot)
+log=math.log(n)
+print("Logarithm : ",log)
+sin=math.sin(n)
+print("Sine :",sin)
